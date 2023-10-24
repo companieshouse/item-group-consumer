71f7c07a9098398d84fd7e924d9dc47a04c5c165
tombradley1 <tbradley@companieshouse.gov.uk>
2023-10-24 09:17:44 +0100

    add structured logging

---
+++ src/main/java/uk/gov/companieshouse/itemgroupconsumer/logging/LoggingUtils.java
@@ -0,0 +1,29 @@
+package uk.gov.companieshouse.itemgroupconsumer.logging;
+
+import uk.gov.companieshouse.logging.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoggingUtils {
+
+    private final Logger logger;
+
+    public LoggingUtils(Logger logger) {
+        this.logger = logger;
+    }
+
+    public Map<String, Object> createLogMap() {
+        return new HashMap<>();
+    }
+
+    public void logIfNotNull(Map<String, Object> logMap, String key, Object loggingObject) {
+        if (loggingObject != null) {
+            logMap.put(key, loggingObject);
+        }
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
+}
